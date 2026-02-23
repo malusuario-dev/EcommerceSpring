f2b9654828a6df1203b789831e5786f54350b1aa
jsanabria-dev <metagolen2003@gmail.com>
2026-02-22 23:47:57 -0500

    feat: agregar el update image

--- src/main/java/com/example/Ecomerce/common/application/Request.java
+++
@@ -1,4 +0,0 @@
-package com.example.Ecomerce.common.application;
-
-public interface Request <T>{
-}
--- src/main/java/com/example/Ecomerce/common/application/RequestHandler.java
+++
@@ -1,7 +0,0 @@
-package com.example.Ecomerce.common.application;
-
-
-public interface RequestHandler <T extends Request<R>, R>{
-    R handle(T request);
-    Class<T> getRequestType();
-}
